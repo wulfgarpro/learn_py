4871039e9f201b1f745640604021598e0073000e
James Fraser <wulfgar.pro@gmail.com>
2018-06-02 21:27:23 +1000

    autopep8 changes

--- distutils/new_setuptools/MyApp/setup.py
+++ distutils/new_setuptools/MyApp/setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
-setup(  
-        name="myapp",
-        version="1.0",
-        description="Example using setuptools",
-        author="James Fraser",
-        author_email="wulfgar.pro@gmail.com",
-        url="https://www.wulfgar.pro",
-        packages=find_packages(),
-        install_requires='cool_math3',
-        entry_points={
-            'console_scripts': [
-                'go = myapp.myapp:main'
-                ]
-            }
+setup(
+    name="myapp",
+    version="1.0",
+    description="Example using setuptools",
+    author="James Fraser",
+    author_email="wulfgar.pro@gmail.com",
+    url="https://www.wulfgar.pro",
+    packages=find_packages(),
+    install_requires='cool_math3',
+    entry_points={
+        'console_scripts': [
+            'go = myapp.myapp:main'
+        ]
+    }
 )
